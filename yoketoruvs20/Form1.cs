00d6e1a59208ea07f3b53fb24b36c8bd1a2d9167
Yu Tanaka <YRK00337@nifty.com>
2020-09-24 10:27:47 +0900

    9/24課題完了

--- yoketoruvs20/Form1.cs
+++ yoketoruvs20/Form1.cs
@@ -101,9 +101,32 @@ namespace yoketoruvs20
         {
             Point mp = PointToClient(MousePosition);
 
-            // TODO: mpがプレイヤーの中心になるように設定
+            // mpがプレイヤーの中心になるように設定
+            chrs[PlayerIndex].Left = mp.X - chrs[PlayerIndex].Width / 2;
+            chrs[PlayerIndex].Top = mp.Y - chrs[PlayerIndex].Height / 2;
 
+            for(int i = EnemyIndex; i < ChrMax; i++)
+            {
+                chrs[i].Left += vx[i];
+                chrs[i].Top += vy[i];
 
+                if (chrs[i].Left < 0)
+                {
+                    vx[i] = Math.Abs(vx[i]);
+                }
+                if (chrs[i].Top < 0)
+                {
+                    vy[i] = Math.Abs(vy[i]);
+                }
+                if (chrs[i].Right > ClientSize.Width)
+                {
+                    vx[i] = -Math.Abs(vx[i]);
+                }
+                if (chrs[i].Bottom > ClientSize.Height)
+                {
+                    vy[i] = -Math.Abs(vy[i]);
+                }
+            }
         }
 
         void initProc()
@@ -133,6 +156,8 @@ namespace yoketoruvs20
                     {
                         chrs[i].Left = rand.Next(ClientSize.Width - chrs[i].Width);
                         chrs[i].Top = rand.Next(ClientSize.Height - chrs[i].Height);
+                        vx[i] = rand.Next(-SpeedMax, SpeedMax + 1);
+                        vy[i] = rand.Next(-SpeedMax, SpeedMax + 1);
                     }
 
                     break;
